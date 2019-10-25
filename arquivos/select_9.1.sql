3e0db5dae94f0bad9d214763aa4973fdb9670b2b
kamairen <55880506+kamairen@users.noreply.github.com>
2019-10-25 20:49:30 -0300

    Update select_9.1.sql

--- arquivos/select_9.1.sql
+++ arquivos/select_9.1.sql
@@ -1,14 +1,42 @@
+import psycopg2
+conn = psycopg2.connect(host = "tuffi.db.elephantsql.com", port = "5432", database = "epiisgzj", user = "epiisgzj", password = "rPcFrCdmPfIHaGYn_j105uSkXOf1Q2up")
+cur = conn.cursor()
 
-select * from Jogador
+print("Tabela Jogador")
+cur.execute("""select * from Jogador""")
+result = cur.fetchall()
+for i in result:
+  print(i)
+  
+print("\n")
+print("Tabela Time")
 
-select * from Time
+cur.execute("""select * from Time""")
+result = cur.fetchall()
+for i in result:
+  print(i)
+print("\n")
 
-select * from Partida
+print("Tabela Partida")
+cur.execute("""select * from Partida""")
+result = cur.fetchall()
+for i in result:
+  print(i)
+print("\n")
 
-select * from Registro
+print("Tabela Registro")
+cur.execute("""select * from Registro""")
+result = cur.fetchall()
+for i in result:
+  print(i)
+print("\n")
 
-select * from Chat
-
+print("Tabela Chat")
+cur.execute("""select * from Chat""")
+result = cur.fetchall()
+for i in result:
+  print(i)
+print("\n")
 
 print("Tabela Jogador_Time")
 cur.execute("""select * from Jogador_Time""")
