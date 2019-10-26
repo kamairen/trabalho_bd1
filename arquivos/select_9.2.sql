605a40aab8d1ae6d1ac2c063547fab8d9162c6f4
kamairen <55880506+kamairen@users.noreply.github.com>
2019-10-25 21:09:31 -0300

    Update select_9.2.sql

--- arquivos/select_9.2.sql
+++ arquivos/select_9.2.sql
@@ -3,28 +3,25 @@ cur.execute("""select * from jogador where sexo = 'M'""")
 result = cur.fetchall()
 for i in result:
   print(i)
-  
-print("\n")
-#Seleciona todas as partidas de futebol
-cur.execute("""select * from partida where modalidade = 'Futebol'""")
-result = cur.fetchall()
-for i in result:
-  print(i)
-  
 print("\n")
 
-#Seleciona todos os registros de partidas que acabaram 0 a 0
-cur.execute("""select * from registro where placar_proponente= 0 and placar_convidado=0""")
+#Seleciona modalidade futebol
+cur.execute("""select * from modalidade where nome = 'Futebol'""")
+result = cur.fetchall()
+for i in result:
+  print(i)  
+print("\n")
+
+#Seleciona todos os registros de partidas que acabaram 2 a 2
+cur.execute("""select * from registro where placar_proponente= 2 and placar_convidado=2""")
 result = cur.fetchall()
 for i in result:
   print(i)
-  
 print("\n")
 
 #Selecion todos os Ids de jogadores que são participantes do time com id 5
 cur.execute("""select * from jogador_time where id_Time= 5""")
 result = cur.fetchall()
 for i in result:
-  print(i)
-  
+  print(i)  
 print("\n")
